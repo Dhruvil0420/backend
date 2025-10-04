60df1335f3b17a121fc9ceda8f26750eebfa971b
Dhruvil0420 <dhruvilparmar1819@gmail.in>
2025-10-04 17:59:26 +0530

    monodb connect

---
+++ src/index.js
@@ -0,0 +1,45 @@
+// require('dotenv').config({path: './env'})
+import dotenv from 'dotenv';
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: './env'
+});
+
+
+connectDB();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const app = express();
+// ;(async () => {
+//     try {
+//         await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+//         app.on("error",(error) => {
+//             console.log("Error :",error);
+//             throw error;
+//         });
+
+//         app.listen(process.env.PORT,()=> {
+//             console.log(`App is listeing On Port:${process.env.PORT}`);
+//         })
+//     } 
+//     catch(error) {
+//         console.log("Error: ",error);
+//         throw error;
+//     }
+// })()
